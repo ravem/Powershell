6ee1b8630860ffe920206e3ff9459161b1a8832f
ravem <transalp98@gmail.com>
2019-11-05 13:26:12 +0100

    Update LogCollector.ps1

--- LogCollector.ps1
+++ LogCollector.ps1
@@ -1,8 +1,8 @@
 #Export System configuration data, Minidump folder and Event log files for troubleshooting
 
-#Credits to 		https://serverfault.com/users/511250/blayderunner123
-#			https://github.com/piesecurity
-#			if your name missing in the credits drop me a line
+#Credits to 				https://blogs.msdn.microsoft.com/virtual_pc_guy
+#					https://github.com/piesecurity
+#					if your name missing in the credits drop me a line
 
 
 [CmdletBinding()]
@@ -18,53 +18,76 @@ Param (
 )
 
 
-Write-Host "Starting script..."
+#Get the location path for the script execution
+$PSScriptRoot
 
 
 #Verify that user running script is an administrator
 
-$IsAdmin=[Security.Principal.WindowsIdentity]::GetCurrent()
-If ((New-Object Security.Principal.WindowsPrincipal $IsAdmin).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE)
-    {
-       "`nYOU ARE NOT A LOCAL ADMINISTRATOR. `nI'll try to launch this script as administrator..."
-        # We are not running "as Administrator" - so we relaunch as administrator
-        # Create a new process object that starts PowerShell
-        $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+   }
+else
+   {
+   # We are not running "as Administrator" - so relaunch as administrator
 
-        # Specify the current script path and name as a parameter
-        $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+   }
+ 
+# Let's start
 
-        # Indicate that the process should be elevated
-        $newProcess.Verb = "runas";
 
-        # Start the new process
-        [System.Diagnostics.Process]::Start($newProcess);
+Write-Host "Starting script..."
 
-        # Exit from the current, unelevated, process
-        exit
-    }
+
+#Changing working directory
+Set-Location -Path $PSScriptRoot
 
 
 #Checking if the output directory exists
-
-Write-Verbose "Checking For Output Directory"
+Write-Host "Checking For Output Directory "
 if (!(test-path $output)) 
 	{
-		Write-Verbose "Creating Output Directory $output"
+		Write-Host "Creating Output Directory $output"
 		mkdir $output | Out-Null
 	}
 
 
 #Exporting System configuration
 
-Write-Verbose "Checking system configuration"
+Write-Host "Checking System Configuration"
 	Get-ComputerInfo > $output\ComputerInfo.txt
 	Get-ComputerInfo | Select-Object -ExpandProperty OSHotFixes > $output\Hotfixes.txt
 
 
 #Exporting ACL configuration for application/s folder
 
-Write-Verbose "Checking Applicatin folder ACL configuration"
+Write-Host "Checking Application Folder ACL Configuration"
 $path = "c:\share" #define path to the shared folder
 $reportpath ="$output\ACL.csv" #define path to export permissions report
 #script scans for directories under shared folder and gets acl(permissions) for all of them
@@ -72,7 +95,7 @@ dir -Recurse $path | where { $_.PsIsContainer } | % { $path1 = $_.fullname; Get-
 
 
 #Archive the minidump folder
-
+Write-Host "Archiving Minidump files, if available"
 $source = "c:\windows\MiniDump"
 $destination = "$output\minidump_folder.zip"
 
@@ -85,7 +108,7 @@ if(Test-path $destination) {Remove-item $destination}
 if ($excludeEvtxFiles) {
     $excludeEvtxFiles | ForEach-Object {
         $LogName = "$LogTag-" + $_
-        Write-Verbose "Dumping $_ Event Log to CSV"
+        Write-Host "Dumping $_ Event Log to CSV"
         Try {
             Get-EventLog $_ -ErrorAction Stop |
                 select @{name="containerLog";expression={$LogName}},
@@ -101,29 +124,29 @@ if ($excludeEvtxFiles) {
                Export-Csv -NoTypeInformation ($output + "\" + "$LogTag-" + $_ + ".csv")
         }
         Catch {
-            Write-Verbose "Previous Log doesn't have any records. No output will be produced"
+            Write-Host "Previous Log doesn't have any records. No output will be produced"
         }
         
     }
 }
-Write-Verbose "Dump All Operational Logs Event Log With Tag: $logtag Excluding: $excludeEvtxFiles"
+Write-Host "Dump All Operational Logs Event Log With Tag: $logtag Excluding: $excludeEvtxFiles"
 
 Get-WinEvent -ListLog * | where-object {$_.recordcount -gt 0} | where-object {$excludeEvtxFiles -notcontains $_.LogName} |
 ForEach-Object {
     wevtutil epl $_.LogName  ($output + "\" + "$LogTag-" + ($_.LogName -replace "/","%4") +".evtx")
 }
 
-Write-Verbose "Adding Event Context to exported evtx files"
+Write-Host "Adding Event Context to exported evtx files"
 Get-ChildItem $output\*.evtx | ForEach-Object {
     wevtutil archive-log $_ /l:en-us
 }
 
 if ($IncludeAllEvtxFiles) {
-    Write-Verbose "Gathering Evtx Files for all previously excluded files"
+    Write-Host "Gathering Evtx Files for all previously excluded files"
     $excludeEvtxFiles | ForEach-Object {
         wevtutil epl $_ ($output + "\" + "$LogTag-" + ($_ -replace "/","%4") +".evtx")
     }
-    Write-Verbose "Adding Event Context to previously excluded files"
+    Write-Host "Adding Event Context to previously excluded files"
     $excludeEvtxFiles | foreach-object {
         Get-ChildItem ($output +"\" + $LogTag + "-" + $_ + ".evtx") | ForEach-Object {
             wevtutil archive-log $_ /l:en-us
